4b74cd8abbc42e1a77923f9438e7e44830fc0999
Roy-wonji <shuwj81@daum.net>
2022-03-17 21:45:48 +0900

    ✨[feat] : 각 레이블 및 버튼에 액션 함수 추가

--- Calculator/Calculator/Controllers/CalcultorViewController.swift
+++ Calculator/Calculator/Controllers/CalcultorViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numberOutputLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func NumberButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func clearButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func additionSubtractionOperatorButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func percentageButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func divideButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func mutiplyButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func subtractButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func equalButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func dotButtonTapped(_ sender: UIButton) {
+        
+    }
+    
 }
 
