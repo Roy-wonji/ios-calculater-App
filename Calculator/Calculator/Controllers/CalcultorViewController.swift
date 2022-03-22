e4aab9720c6bac796ddd359fe504bcf324fc7b32
Roy-wonji <shuwj81@daum.net>
2022-03-22 21:43:49 +0900

    ♻️[refator]: 전체적으로 코드 리팩토링

--- Calculator/Calculator/Controllers/CalcultorViewController.swift
+++ Calculator/Calculator/Controllers/CalcultorViewController.swift
@@ -8,8 +8,6 @@
 import UIKit
 
 final class CalcultorViewController: UIViewController {
-    static let shared: CalcultorViewController = CalcultorViewController()
-    
     @IBOutlet weak var numberOutputLabel: UILabel!
     
     override func viewDidLoad() {
@@ -18,71 +16,45 @@ final class CalcultorViewController: UIViewController {
     }
     
     @IBAction private func NumberButtonTapped(_ sender: UIButton) {
-        guard let numberValue = sender.title(for: .normal) else  {return}
-        if  InitialValue.displayNumberLabel.count < 9 {
-            InitialValue.displayNumberLabel += numberValue
-            numberOutputLabel.text = InitialValue.displayNumberLabel
-        }
+       
     }
     
     @IBAction private func clearButtonTapped(_ sender: UIButton) {
-        makeZeroValue()
-        numberOutputLabel.text =  "0"
+       
     }
     
     @IBAction private func additionSubtractionOperatorButtonTapped(_ sender: UIButton) {
-        operation(.additionSubtractionOperator)
+       
     }
     
     @IBAction private func percentageButtonTapped(_ sender: UIButton) {
-        operation(.percentage)
+        
     }
     
     @IBAction private func divideButtonTapped(_ sender: UIButton) {
-        operation(.divide)
+       
     }
     
     @IBAction private func mutiplyButtonTapped(_ sender: Any) {
-        operation(.mutiply)
+       
     }
     
     @IBAction private func subtractButtonTapped(_ sender: UIButton) {
-        operation(.subtract)
+       
     }
     
     @IBAction private func addButtonTapped(_ sender: UIButton) {
-        operation(.add)
+     
     }
     
     @IBAction private func equalButtonTapped(_ sender: UIButton) {
-        operation(InitialValue.currentOperation)
+     
     }
     
     @IBAction private func dotButtonTapped(_ sender: UIButton) {
-        makeDotValue()
-        numberOutputLabel.text = InitialValue.displayNumberLabel
+      
     }
     
-    private func operation( _ operation: OperationCalculator) {
-        if InitialValue.currentOperation != .unknown {
-            if !InitialValue.displayNumberLabel.isEmpty {
-                InitialValue.newCalculation = InitialValue.displayNumberLabel
-                InitialValue.displayNumberLabel = ""
-                opearationValue()
-                if let result = Double(InitialValue.result), result.truncatingRemainder(dividingBy: 1) == 0 {
-                    InitialValue.result = "\(Int(result))"
-                }
-                
-                InitialValue.firstCalculation = InitialValue.result
-                numberOutputLabel.text = InitialValue.result
-            }
-            InitialValue.currentOperation = operation
-            
-        } else {
-            InitialValue.firstCalculation = InitialValue.displayNumberLabel
-            InitialValue.currentOperation = operation
-            InitialValue.displayNumberLabel = ""
-        }
-    }
+   
 }
 
